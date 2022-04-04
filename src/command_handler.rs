c33cec9288608d3eca26cf76e840e664e34960cd
Aya-Komichi <ayaya.mosaic@gmail.com>
2022-04-04 12:20:04 +0100

    Added the dotenv dependency. The bot starts but doesn't respond to messages yet.

--- src/command_handler.rs
+++ src/command_handler.rs
@@ -9,10 +9,10 @@ pub struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, cx: Context, msg: Message) {
-        let prefix = env::var("PREFIX").expect("No prefix found in .env.");
+        let prefix = env::var("PREFIX").unwrap();
         let content = msg.content.to_string();
 
-        if content.starts_with(&prefix) {
+        if content.starts_with(&prefix[..]) {
             let command = content[prefix.len()..content.len() - 1].trim();
             if command == "ping" {
                 msg.reply(&cx, "Hello! I'm here!", true).await.ok();
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ mod command_handler;
 
 use {
     ruvolt::{Client, Result},
+    dotenv::dotenv,
     std::env
 };
 
 #[tokio::main]
 async fn main() -> Result {
-    let token = env::var("TOKEN").expect("No token found in .env.");
+    dotenv().ok();
+    let token = env::var("TOKEN").unwrap();
     let mut client = Client::new(command_handler::Handler, token).await?;
 
     client.listen().await
