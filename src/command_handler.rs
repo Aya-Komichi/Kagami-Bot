76fab6864b2d54d9354e423daf019e5613254f35
Aya-Komichi <ayaya.mosaic@gmail.com>
2022-04-05 23:19:09 +0100

    Added a simple error handler.

--- src/command_handler.rs
+++ src/command_handler.rs
@@ -1,6 +1,6 @@
 use {
     async_trait::async_trait,
-    ruvolt::{models::Message, models::events::ReadyEvent, Context, EventHandler},
+    ruvolt::{models::Message, models::events::ReadyEvent, Context, EventHandler, error::Error},
     dotenv::dotenv,
     std::env
 };
@@ -20,7 +20,7 @@ impl EventHandler for Handler {
 
         if content.starts_with(&prefix[..]) {
             let command = content[prefix.len()..content.len()].trim().to_lowercase();
-            let _result = match command.as_str() {
+            let result = match command.as_str() {
                 "ping" => msg.reply(&cx, "Hello! I'm here!", true).await,
                 "shutdown" => {
                     msg.reply(&cx, "Shutting down. See you later!", true).await.ok();
@@ -28,6 +28,16 @@ impl EventHandler for Handler {
                 },
                 _ => Ok(msg),
             };
+            handle_error(result, content).await;
         }
+    }
+}
+
+async fn handle_error(result: Result<Message, Error>, msg_content: String) {
+    if result.is_err() {
+        let error: Error = result.err().unwrap();
+        println!("An error ocurred when executing a command.");
+        println!("Message content: {}", msg_content);
+        eprintln!("{}", error);
     }
 }
