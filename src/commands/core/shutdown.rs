80b4297830aaef8ad386259464e238334dc9b97f
Aya-Komichi <ayaya.mosaic@gmail.com>
2022-05-21 20:54:57 +0100

    The shutdown command was brought back since the delta rust backed was now fixed.

--- src/commands/core/shutdown.rs
+++ src/commands/core/shutdown.rs
@@ -1,6 +1,6 @@
 use ruvolt::{models::Message, Context, Result, error::Error};
 
 pub async fn shutdown(cx: &Context, msg: Message) -> Result<Message, Error> {
-    msg.reply(cx, "This command is still unfinished.", true).await
-    //std::process::exit(0)
+    msg.reply(cx, "Shutting down. See you later!", true).await.ok();
+    std::process::exit(0)
 }
